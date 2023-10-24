456d5dc838320a72b4da9ec748a629c65d875f7b
nmedehalreddy <143042186+nmedehalreddy@users.noreply.github.com>
2023-10-23 20:03:29 -0400

    finish

---
+++ itis3135/scripts/animal.js
@@ -0,0 +1,23 @@
+
+function myAnimal() {
+    alert("My Favorite Animal is Panda");
+}
+function animalColor() {
+    alert("The color of the Panda is black and white");
+}
+function animalHeight() {
+    alert("The height of the Panda is 5 feet");
+}
+function animalSize() {
+    alert("The size of the Panda is 275 punds");
+}
+function animalFood() {
+    alert("The food the Panda eats is bamboo");
+}
+
+
+
+
+
+
+
--- itis3135/scripts/firstscripts.js
+++ itis3135/scripts/firstscripts.js
@@ -19,5 +19,50 @@ function myFunction() {
     document.getElementById("demo").innerHTML = text;
   }
 
+  function alertPolygonName() {
+    const thenumber = Number(document.getElementById("number").value);
+    //Make sure thenumbver is absolute and save it in a new variable 
+   var absolutenumber = Math.abs(thenumber);
+    //make sure that the number is rounded ex 4.6 -> 5and save it in a new variable 
+    var newnumber = Math.round(absolutenumber);
+
+    switch (Number(newnumber)) {
+      case 1:
+        alert("henagon");
+        break;
+      case 2:
+        alert("digon");
+        break;
+      case 3:
+        alert("trigon");
+        break;
+      case 4:
+        alert("tetragon");
+        break;
+      case 5:
+        alert("pentagon");
+        break;
+      case 6:
+        alert("hexagon");
+        break;
+      case 7:
+        alert("heptagon");
+        break;
+      case 8:
+        alert("octagon");
+        break;
+      case 9:
+        alert("enneagon");
+        break;
+      case 10:
+        alert("decagon");
+        break;
+        default: 
+        alert("1 to 10") 
+    // code block
+
+    }
+  }
+
 
  
