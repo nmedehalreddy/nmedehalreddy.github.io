b80994ad240e8a1ed978e1c1ec83e00520caa815
nmedehalreddy <143042186+nmedehalreddy@users.noreply.github.com>
2023-12-07 22:43:42 -0500

    finish

--- itis3135/Activity14/MedehalReddy-Activity14.js
+++ itis3135/Activity14/MedehalReddy-Activity14.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-	
+    $("#nav_list li").click(function() {
+        $.getJSON("json_files/" + $(this).children("a").attr("title") + ".json", function(data) {
+        data = data['speakers'][0];
+        $("main h1").html(data.title);
+        $("main h2").html(data.month);
+        $("main h3").html(data.speaker);
+        $("main p").html(data.text);
+        $("main img").attr("src", data.image);
+        });
+    });
 }); // end ready
+$(window).on( "load", function() {
+    $.getJSON("json_files/" + $(this).children("a").attr("title") + ".json", function(data) {
+    data = data['speakers'][0];
+    $("main h1").html(data.title);
+    $("main h2").html(data.month);
+    $("main h3").html(data.speaker);
+    $("main p").html(data.text);
+    $("main img").attr("src", data.image);
+    });
+});
